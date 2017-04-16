49914b39be6910c9592a9064a3c52d295adcdf65
Sebastian Pinegger <sebastian.pinegger@gmx.ch>
2017-04-16 14:39:22 +0200

    added check for wwwroot folder if exists

--- gulpfile.js
+++ gulpfile.js
@@ -2,34 +2,41 @@ var gulp = require('gulp');
 var spawn = require('child_process').spawn;
 var watch = require('gulp-watch');
 var batch = require('gulp-batch');
+var fs = require('fs');
 
 // restore backend
-gulp.task('restore backend', function (done) {
-    return spawn('dotnet', ['restore'],{ cwd: 'backend/' });
-    done();
+gulp.task('restore backend', function () {
+
+    return spawn('dotnet', ['restore'], { cwd: 'backend/' });
 });
 
 // restore backend
-gulp.task('install dependencies frontend', function (done) {
-    spawn('npm', ['install'], { cwd: 'frontend/' });
-    done();
+gulp.task('install dependencies frontend', function () {
+    return spawn('npm', ['install'], { cwd: 'frontend/' });
 });
 
 // call of dotnet watch run
 gulp.task('build backend', function () {
-    spawn('dotnet', ['watch', 'run'],{ cwd: 'backend/' });
-});
-
-// call of ng build
-gulp.task('build blabla', function () {
-    return watch('frontend/**/*.ts',{ ignoreInitial: false } ).pipe(spawn('ng', ['build'], { cwd: 'frontend/' }));
+    function startServer(){
+            console.log('Starting server at http:/localhost:5000');
+    return spawn('dotnet', ['watch', 'run'],{ cwd: 'backend/' });
+    }
+    if (!fs.exists('/backend/wwwroot/')) {
+        console.log('wwwroot non-existing and ng build is being run');
+        ngBuild = spawn('ng', ['build'], { cwd: 'frontend/' });
+        ngBuild.on('exit', function(){
+            startServer();
+        });
+    } else {
+        startServer();
+    }
 });
 
 // build and copy frontend
 gulp.task('build frontend', function () {
     gulp.watch('frontend/**/*.ts').on('change', function() {
         console.log('Changes in the Angular App detected.');
-        spawn('ng', ['build'], { cwd: 'frontend/' });
+        return spawn('ng', ['build'], { cwd: 'frontend/' });
     });
 });
 
